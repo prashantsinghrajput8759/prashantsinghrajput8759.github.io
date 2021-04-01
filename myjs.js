8fa07c7fb2a323e244d4b6edeb95572a8a669549
prashant singh <singhprashant8759@gmail.com>
2021-04-01 17:30:25 +0530

    more responsive

--- myjs.js
+++ myjs.js
@@ -3,3 +3,11 @@ window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY > 0);
 });
+
+function menutoggle(){
+
+    var toggle=document.querySelector(".toggle");
+    var menu=document.querySelector(".menu");
+    toggle.classList.toggle("active");
+    menu.classList.toggle("active");
+}
